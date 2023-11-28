df6e935fceeb2a556f440c332d38b5088b9229c1
kimhyun <forhn37@gmail.com>
2023-11-28 11:53:13 +0900

    프로젝트 예제 작성

---
+++ app.js
@@ -0,0 +1,41 @@
+function createElement(type, props, ...children) {
+  return { type, props, children }
+}
+function component(stateData) {
+  const menuItems = [];
+  for (let i = 0; i < stateData.length; i++) {
+    const item = stateData[i]
+    const menuItem = createElement('li', {}, createElement('a', { href: item.hash }, item.text))
+    menuItems.push(menuItem)
+  }
+  const menu = createElement('ul', {}, ...menuItems)
+  const content = createElement('div', {}, 'Hello React')
+  return createElement('div', {}, menu, content)
+}
+function render(virtualDom) {
+  if (typeof virtualDom === 'string') {
+    return document.createTextNode(virtualDom)
+  }
+  const element = document.createElement(virtualDom.type)
+  if (virtualDom.props) {
+    for (const [key, value] of Object.entries(virtualDom.props)) {
+      element.setAttribute(key, value)
+    }
+  }
+  for (let i = 0; i < virtualDom.children.length; i++) {
+    const child = virtualDom.children[i]
+    element.appendChild(render(child))
+  }
+  return element
+}
+const stateData = [
+  { hash: '#home', text: 'Home' },
+  { hash: '#about', text: 'About' },
+  { hash: '#services', text: 'Services' },
+  { hash: '#portfolio', text: 'portfolio' },
+  { hash: '#contact', text: 'Contact' }
+];
+
+const virtualDom = component(stateData)
+const container = document.getElementById('root')
+container.appendChild(render(virtualDom))
